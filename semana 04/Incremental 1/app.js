7e96eb17b9d82d9ef1bc69316f8170dfc8525529
Xurde <xurdebrandulas@gmail.com>
2023-03-28 13:41:32 +0200

    incremental 2 semana 4

--- semana 04/Incremental 1/app.js
+++ semana 04/Incremental 1/app.js
@@ -1,21 +1,39 @@
 var botonLikes = document.getElementById ("boton_likes");
 var totalLikes = document.getElementById ("total_likes");
 
-let cuentaLikes = 5; //likes ya acumulados en la publicación
-totalLikes.textContent = cuentaLikes + " Me gusta";
+let cuentaLikes = 3; //likes ya acumulados en la publicación
+totalLikes.innerHTML = cuentaLikes + " Me gusta";
 let i="0"
 
-boton_likes.addEventListener("click", () => {
+botonLikes.addEventListener("click", () => {
     if (i == 0) { 
     i++
     cuentaLikes++;
-    totalLikes.textContent = cuentaLikes + " Me gusta";
+    // totalLikes.innerHTML = cuentaLikes + " Me gusta";
     botonLikes.style.backgroundColor = "#3FBEE7"
     }
     else{
         i--
         cuentaLikes--;
-        totalLikes.textContent = cuentaLikes + " Me gusta";
+        // totalLikes.innerHTML = cuentaLikes + " Me gusta";
         botonLikes.style.backgroundColor = ""
      }
+     totalLikes.innerHTML = cuentaLikes + " Me gusta";
+});
+
+
+
+
+
+var deleteAccount = document.getElementById ("eliminar");
+
+let usuarios = ["David", "John"] 
+console.log (usuarios)
+
+deleteAccount.addEventListener ("click", () => {
+  let confirmacion = confirm("Si elimina su cuenta no podrá recuperar sus publicaciones ni mensajes \n ¿Está conforme con esto?")
+  if (deleteAccount != null){
+    usuarios.pop();
+  }
+
 });
---
+++ semana 04/incremental 2/app.js
@@ -0,0 +1,17 @@
+var deleteAccount = document.getElementById ("eliminar");
+
+let usuarios = ["David", "John"] 
+console.log (usuarios)
+
+deleteAccount.addEventListener ("click", () => {
+  let confirmacion = confirm("Si elimina su cuenta no podrá recuperar sus publicaciones ni mensajes \n¿Está conforme con esto?")
+  if (confirmacion == true ){
+    usuarios.pop();
+    console.log (usuarios)
+    window.open ("login.html")
+  }else{
+  alert("No has eliminado la cuenta.\nGracias por continuar con nosotros.")
+  }
+
+});
+ 
