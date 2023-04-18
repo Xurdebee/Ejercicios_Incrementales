0535dd6980cb75617d4e3b8af641181c01219d5e
Xurde <xurdebrandulas@gmail.com>
2023-04-18 18:47:26 +0200

    Update programa.js

--- semana 06/incremental 02/programa.js
+++ semana 06/incremental 02/programa.js
@@ -3,3 +3,21 @@ var cursos = require('./cursos_usuario');
 cursos.forEach(function (cursos) {
     console.log(cursos);
 });
+
+
+const{faker} = require('@faker-js/faker');
+
+function createRandomUser() {
+    return {
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    };
+  }
+
+const user = createRandomUser();
+console.log (user)
